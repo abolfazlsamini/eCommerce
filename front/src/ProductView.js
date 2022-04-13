349ceec9302cb57e98f00cedf99cd861d0138dbf
saminimaster <samini7a@gmail.com>
2022-04-13 04:46:25 -0330

    first commit

--- front/src/ProductView.js
+++ front/src/ProductView.js
@@ -4,43 +4,40 @@ const { useState } = require("react");
 
 function ProductView(props){
 
-    const location = useLocation();
-    const id = location.state;
-    const [detailedList,setDetailedList] = useState([]);
+  const location = useLocation();
+  const id = location.state;
+  const [detailedList,setDetailedList] = useState({});
 
-    function fetcher(id){
+  function fetcher(id){
 
-        const url = 'http://127.0.0.1:8000/api/products/' + id.id;
-        fetch(url).then(
-          res => {return res.json()}).then(
-            data => setDetailedList(data));
-            
-      }
+    const url = 'http://127.0.0.1:8000/api/products/' + id.id;
+      fetch(url).then(res => {return res.json()}).then(
+        data => {setDetailedList(data);console.log("data",data)});
+        
+    }
 
-    useEffect(()=>{
-    fetcher(id);
-    },[])
+  useEffect(()=>{
+  fetcher(id);
+  },[])
 
-      return(
-          <div>
-              <h1>
-                {console.log(detailedList)}
+  return(
+    <div>
+      <h1>
+        {console.log(detailedList)}
 
-                {detailedList.map(detail => {return (
-
-        <div  >
-          {detail.name}<br/>
-          {detail.price}<br/>
-          {detail.description}
-        </div>
+      <div  className="Product detail">
+        {detailedList.name}<br/>
+        {detailedList.price}<br/>
+        {detailedList.description}
+      </div>
 
 
-        )}
-      )}
 
-              </h1>
-          </div>
-      )
+  
+
+      </h1>
+    </div>
+  )
 }
 
 export default ProductView;
