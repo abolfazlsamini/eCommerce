b9885eefcec4bee85030b4e0fbadcaf9cdcc5ea1
saminimaster <samini7a@gmail.com>
2022-04-14 08:52:22 -0330

    first commit

--- front/src/NavBar.js
+++ front/src/NavBar.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
 
     return ( 
 
-      <div className="NavBar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-
-        </ul>
-
-        <hr />
-
-      </div>
+      <nav className="NavBar">
+      <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "isActive" : "notActive")}
+        >
+          Home
+      </NavLink>
+      <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "isActive" : "notActive")}
+        >
+          About
+      </NavLink>
+      <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? "isActive" : "notActive")}
+        >
+          Contact
+      </NavLink>
+    </nav>
 
      );
 }
