805bdce31e4d357322c9efb3f4fe7458b8bc1f92
saminimaster <samini7a@gmail.com>
2022-04-17 08:12:16 -0330

    first commit

--- front/src/Home.js
+++ front/src/Home.js
@@ -6,27 +6,35 @@ import { Link } from 'react-router-dom';
 function Home() {
 
   const [list, setList] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   function fetcher(){
     fetch('http://127.0.0.1:8000/api/products/').then(
       res => {return res.json()}).then(
-        data => setList(data));
+        data => setList(data)).then(()=>setIsLoading(false));
   }
-
+  
   useEffect(()=>{
     fetcher();
   },[])
 
+const loadingAnimation = 
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
 
   return (
-
     <div className="Home">
       <h3>All Products:</h3>
+
+      {isLoading ? loadingAnimation : console.log("done")}
 
       {list.map(product => {return (
       <li className='ProductList' key={product.id}>
         
-        {/* TODO: make a loading effect for products */}
+        {/* TODO: make a loading effect for products be is the proper position*/}
 
         <Link to= {"/"+product.id } state={{id :product.id}}>
           {product.name}<br/>
