cfb2aa8dc3fb2356936221f855289596b37ba0cd
saminimaster <samini7a@gmail.com>
2022-05-04 16:23:51 -0330

    first commit

--- back/pro/serializers.py
+++ back/pro/serializers.py
@@ -20,10 +20,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'],None)
         user.set_password(validated_data['password'])
+
         return user
+
     #     return super().create(validated_data)
     #     user.setpassword
